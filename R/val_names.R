2be728036b705a745e5d4aa0735cd4722c566770
jchen12065 <60930345+jchen12065@users.noreply.github.com>
2022-03-25 12:47:47 -0400

    Update val_names.R

--- R/val_names.R
+++ R/val_names.R
@@ -6,7 +6,7 @@
 #'
 #' @return pointblank interrogated data.
 #' @export
-#' jchen modification test8
+#' jchen modification test9
 
 val_names <- function(x, expected, ...) {
   agent <- pointblank::create_agent(
